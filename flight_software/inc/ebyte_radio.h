7a2c029060d232aac06b7546dc9b56c8bd4483bf
Angus Eason <aje87@uclive.ac.nz>
2024-07-29 19:31:25 +1200

    uart radio working

---
+++ flight_software/inc/ebyte_radio.h
@@ -0,0 +1,9 @@
+#ifndef EBYTE_RADIO_H
+#define EBYTE_RADIO_H
+
+void radio_uart_init(void);
+void radio_transmit_string(char buff[], int len);
+
+
+
+#endif // EBYTE_RADIO_H
--- flight_software/src/main.c
+++ flight_software/src/main.c
@@ -1,19 +1,29 @@
 #include "common.h"
 #include "gpio.h"
+#include "ebyte_radio.h"
 
 void assert_failed(uint8_t* file, uint32_t line);
+void clock_config(void);
 
 void main(void)
+{
+  clock_config();
+  GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_FAST);
+  radio_uart_init();
+
+  while (1){
+    GPIO_WriteReverse(LED_PORT, LED_PIN);
+    char buff[] = "Hello world!\n";
+    radio_transmit_string(buff, sizeof(buff)/sizeof(buff[0]));
+    delay_ms(1000);
+  }
+}
+
+void clock_config(void)
 {
   CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);
   CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1);
   TIM4_Config();
-  GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_FAST);
-
-  while (1){
-    GPIO_WriteReverse(LED_PORT, LED_PIN);
-    delay_ms(1000);
-  }
 }
 
 

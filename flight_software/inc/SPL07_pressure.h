f946d3ead8601061631b6453307a46543c0b1ff4
Angus Eason <aje87@uclive.ac.nz>
2024-08-12 20:33:47 +1200

    Fixing makefile + i2c write

--- flight_software/inc/SPL07_pressure.h
+++ flight_software/inc/SPL07_pressure.h
@@ -1,7 +1,12 @@
 #ifndef SPL07_PRESSURE_H
 #define SPL07_PRESSURE_H
+#include "common.h"
 
 void spl07_init(void);
 void spl07_test(void);
+
+void i2c_write_byte(uint8_t device_address, uint8_t register_address, uint8_t byte);
+void i2c_read(uint8_t device_address, uint8_t register_address, uint8_t bytes[], uint8_t num_bytes);
+
 
 #endif // SPL07_PRESSURE_H
--- flight_software/src/SPL07_pressure.c
+++ flight_software/src/SPL07_pressure.c
@@ -5,10 +5,23 @@
 
 #define SPL07_MEAS_CFG_ADDR 0x08
 
+#define CONTINUOUS_PRESS_TEMP 0x07
+
 void spl07_init(void)
 {
     I2C_Cmd(ENABLE);
     I2C_Init(80000, 0x69, I2C_DUTYCYCLE_2, I2C_ACK_CURR, I2C_ADDMODE_7BIT, F_CPU/1000000);
+
+    // Configure pressure measurement PRS_CFG
+
+    // Configure temperature measurement TMP_CFG
+
+    // Set measurement mode MEAS_CFG
+    i2c_write_byte(SPL07_CHIP_ADDR, SPL07_MEAS_CFG_ADDR, CONTINUOUS_PRESS_TEMP);
+    char ret_val[1];
+    i2c_read(SPL07_CHIP_ADDR, SPL07_MEAS_CFG_ADDR, ret_val, 1);
+    print_bits_of_byte(ret_val[0]);
+    radio_print("\r\n");
 }
 
 void i2c_read(uint8_t device_address, uint8_t register_address, uint8_t bytes[], uint8_t num_bytes)
@@ -47,6 +60,26 @@ void i2c_read(uint8_t device_address, uint8_t register_address, uint8_t bytes[],
     I2C_AcknowledgeConfig(I2C_ACK_NONE); // Send a nack on next byte recieved to let slaveknow we are done
     while(!I2C_CheckEvent(I2C_EVENT_MASTER_BYTE_RECEIVED)) continue;
     bytes[num_bytes-1] = I2C_ReceiveData();
+    I2C_GenerateSTOP(ENABLE);
+}
+
+void i2c_write_byte(uint8_t device_address, uint8_t register_address, uint8_t byte)
+{
+    // TODO: add timeouts to whiles
+    I2C_GenerateSTART(ENABLE);
+    while(!I2C_CheckEvent(I2C_EVENT_MASTER_MODE_SELECT)) continue;
+
+    I2C_Send7bitAddress(device_address, I2C_DIRECTION_TX);
+    while(!I2C_CheckEvent(I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED)) continue;
+
+    //radio_print("Data\r\n");
+    I2C_SendData(register_address);
+    while(!I2C_CheckEvent(I2C_EVENT_MASTER_BYTE_TRANSMITTED)) continue;
+
+    //radio_print("Data\r\n");
+    I2C_SendData(byte);
+    while(!I2C_CheckEvent(I2C_EVENT_MASTER_BYTE_TRANSMITTED)) continue;
+
     I2C_GenerateSTOP(ENABLE);
 }
 

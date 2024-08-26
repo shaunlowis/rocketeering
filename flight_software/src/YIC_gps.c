ba6fa28fd6688ab7850e0bf9ff8d72cacb79903a
Angus Eason <aje87@uclive.ac.nz>
2024-08-26 22:23:14 +1200

    gps notes

---
+++ flight_software/src/YIC_gps.c
@@ -0,0 +1,8 @@
+// Broadcasts messages in NMEA format
+// Decoder: https://github.com/kosma/minmea
+
+
+/* Change baud rate with PMKT251 
+https://cdn.sparkfun.com/assets/parts/1/2/2/8/0/PMTK_Packet_User_Manual.pdf
+https://forum.arduino.cc/t/changing-gps-baud-rate-with-minimal-nmeagps-and-gpsport/586590/2
+*/

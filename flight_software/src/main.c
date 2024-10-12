c56c8a6735310f850b41f92001f187a964c31a16
slowis <johnny@dawnaerospace.com>
2024-10-13 11:24:56 +1300

    got read and write working, need to now parse data.

--- flight_software/src/main.c
+++ flight_software/src/main.c
@@ -31,17 +31,28 @@ void main(void)
 
   // SPI setup:
   SPI_DeInit();
-  SPI_Init(SPI_FIRSTBIT_MSB,
-           SPI_BAUDRATEPRESCALER_2,
-           SPI_MODE_MASTER,
-           SPI_CLOCKPOLARITY_LOW,
-           SPI_CLOCKPHASE_1EDGE,
-           SPI_DATADIRECTION_2LINES_FULLDUPLEX,
-           SPI_NSS_HARD,
-           1);
+  SPI_Init(
+    SPI_FIRSTBIT_MSB,
+    SPI_BAUDRATEPRESCALER_2, // unsure if correct..
+    SPI_MODE_MASTER,
+    SPI_CLOCKPOLARITY_LOW,
+    SPI_CLOCKPHASE_1EDGE, // verify with timing diagram
+    SPI_DATADIRECTION_2LINES_FULLDUPLEX, // We just need to add a clock at SCK, then read SO.
+    SPI_NSS_SOFT, // Dont think this is needed?
+    1);
   SPI_Cmd(ENABLE);
   radio_print_debug("SPI initialized\r\n");
-  MSD_Init();
+
+  // Thermocouple pins:
+  // "Drive CS low and apply a clock signal at SCK to read the results at SO."
+  GPIO_Init(THERMO_CHIP_PORT, THERMO_CHIP_PIN_SELECT, GPIO_MODE_OUT_PP_HIGH_FAST);
+
+  
+  radio_print_debug("Thermocouple initialized\r\n");
+
+  // read_thermocouple();
+
+  // MSD_Init();
   // radio_print_debug("SD initialized\r\n");
 
   // print_sd_to_radio();
@@ -71,14 +82,33 @@ void main(void)
   while(1)
   {
     GPIO_WriteReverse(GREEN_LED_PORT, GREEN_LED_PIN);
-    // Can pass direct string or sprintf into a char buff and pass into here, like this:
-    // char pbuff[10];
-    // for (int i=0; i<255; i++)
-    // {
-    //   sprintf(pbuff, "%d\r\n", (int)bytes_read[i]);
-    //   radio_print_debug(pbuff);
-    // }
-    radio_print_debug("peepeepoopoo\r\n");
+    
+    // radio_print_debug("peepeepoopoo\r\n");
+    // read_thermocouple();
+
+    // Drive CS low:
+    GPIO_WriteLow(THERMO_CHIP_PORT, THERMO_CHIP_PIN_SELECT);
+
+    // to store the 4x 8bit reads from thermocouple SPI
+    uint8_t thermocouple_data_buff[4];
+    for (int i=0; i<4; i++)
+    {
+      SPI_SendData(0xFF);
+      thermocouple_data_buff[i] = SPI_ReceiveData();
+    }
+
+    // DOES IT WORK
+    char pbuff[100];
+    for (int i=0; i<4; i++)
+    {
+      sprintf(pbuff, "%u ", thermocouple_data_buff[i]);
+      radio_print_debug(pbuff);
+    }
+    radio_print_debug("\r\n");
+
+    // Then drive CS high:
+    GPIO_WriteHigh(THERMO_CHIP_PORT, THERMO_CHIP_PIN_SELECT);
+  
     // read_gps_buffer();
     // update_imu_state();
     // spl07_update_baro();
@@ -90,11 +120,22 @@ void main(void)
   
   while (1){
     GPIO_WriteReverse(GREEN_LED_PORT, GREEN_LED_PIN);
-
     delay_ms(500);
-    
   }
 }
+
+// void read_thermocouple(void)
+// { 
+//   uint8_t thermocouple_data_buff[14];
+//   thermocouple_data_buff = SPI_ReceiveData();
+//     // Can pass direct string or sprintf into a char buff and pass into here, like this:
+//     char pbuff[14];
+//     for (int i=0; i<255; i++)
+//     {
+//       sprintf(pbuff, "%d\r\n", (int)pbuff[i]);
+//       radio_print_debug(pbuff);
+//     }
+// }
 
 void clock_config(void)
 {
